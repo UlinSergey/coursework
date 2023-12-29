cc9cd264a890e02d3701aa8dc00b9d0aa3c1eb41
Sergey Ulin <PYLIN2002@yandex.ru>
2023-12-29 13:06:39 +0300

    курсовая

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,9 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.LinkedHashSet;
 
 public class Main {
     public static void main(String[] args) {
-        String[] ingredients = {"Капуста","Сухарики", "Капуста", "Томат", "Майонез", "Сухарики", "Перец"};
-        System.out.println("Для приготовления салата понадобиться:");
-        System.out.print(duplicates (ingredients));
-    }
 
-    public static String duplicates (String[] ingredients) {
-        LinkedHashSet<String> set = new LinkedHashSet<>();
-        set.addAll(Arrays.asList(ingredients));
-        return String.join(" ", set);
     }
 }
