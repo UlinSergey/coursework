2171755cd3e389c949ca501fe87b7651383fc025
Sergey Ulin <PYLIN2002@yandex.ru>
2023-12-31 12:02:03 +0300

    курсовая

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 import java.util.LinkedHashSet;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main (String[] args){
 
     }
 }
